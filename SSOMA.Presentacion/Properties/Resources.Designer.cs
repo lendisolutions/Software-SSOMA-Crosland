c631a3e592162d67aa81a768bee9987b0eb045e8
lendisolutions <46729497+lendisolutions@users.noreply.github.com>
2019-02-07 16:51:12 -0500

    07022019 1651

--- SSOMA.Presentacion/Properties/Resources.Designer.cs
+++ SSOMA.Presentacion/Properties/Resources.Designer.cs
@@ -943,6 +943,16 @@ namespace SSOMA.Presentacion.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Pregunta_32x32 {
+            get {
+                object obj = ResourceManager.GetObject("Pregunta_32x32", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Print_16x16 {
             get {
                 object obj = ResourceManager.GetObject("Print_16x16", resourceCulture);
