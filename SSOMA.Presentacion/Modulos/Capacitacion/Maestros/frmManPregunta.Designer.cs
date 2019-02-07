f9bcdfcedf4c41699249cdf7785be2044eee8635
lendisolutions <46729497+lendisolutions@users.noreply.github.com>
2019-02-07 15:25:57 -0500

    07022019 1525

--- SSOMA.Presentacion/Modulos/Capacitacion/Maestros/frmManPregunta.Designer.cs
+++ SSOMA.Presentacion/Modulos/Capacitacion/Maestros/frmManPregunta.Designer.cs
@@ -29,10 +29,299 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmManPregunta));
+            this.tlbMenu = new SSOMA.Presentacion.ControlUser.UIToolBar();
+            this.splitContainerControl1 = new DevExpress.XtraEditors.SplitContainerControl();
+            this.tvwDatos = new System.Windows.Forms.TreeView();
+            this.splitContainerControl2 = new DevExpress.XtraEditors.SplitContainerControl();
+            this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
+            this.chkSelecciona = new DevExpress.XtraEditors.CheckEdit();
+            this.btnBuscar = new DevExpress.XtraEditors.SimpleButton();
+            this.txtDescripcion = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.gcPregunta = new DevExpress.XtraGrid.GridControl();
+            this.gvPregunta = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.gridColumn2 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn1 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn5 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn3 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.repositoryItemCheckEdit1 = new DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit();
+            this.gridColumn7 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn4 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.imgLista = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl1)).BeginInit();
+            this.splitContainerControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl2)).BeginInit();
+            this.splitContainerControl2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
+            this.groupControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chkSelecciona.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtDescripcion.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gcPregunta)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvPregunta)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemCheckEdit1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tlbMenu
+            // 
+            this.tlbMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tlbMenu.Ensamblado = "";
+            this.tlbMenu.Location = new System.Drawing.Point(0, 0);
+            this.tlbMenu.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tlbMenu.Name = "tlbMenu";
+            this.tlbMenu.Size = new System.Drawing.Size(1752, 30);
+            this.tlbMenu.TabIndex = 32;
+            // 
+            // splitContainerControl1
+            // 
+            this.splitContainerControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerControl1.Location = new System.Drawing.Point(0, 30);
+            this.splitContainerControl1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.splitContainerControl1.Name = "splitContainerControl1";
+            this.splitContainerControl1.Panel1.Controls.Add(this.tvwDatos);
+            this.splitContainerControl1.Panel1.Text = "Panel1";
+            this.splitContainerControl1.Panel2.Controls.Add(this.splitContainerControl2);
+            this.splitContainerControl1.Panel2.Text = "Panel2";
+            this.splitContainerControl1.Size = new System.Drawing.Size(1752, 604);
+            this.splitContainerControl1.SplitterPosition = 617;
+            this.splitContainerControl1.TabIndex = 33;
+            this.splitContainerControl1.Text = "splitContainerControl1";
+            // 
+            // tvwDatos
+            // 
+            this.tvwDatos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tvwDatos.Location = new System.Drawing.Point(0, 0);
+            this.tvwDatos.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tvwDatos.Name = "tvwDatos";
+            this.tvwDatos.Size = new System.Drawing.Size(617, 604);
+            this.tvwDatos.TabIndex = 2;
+            // 
+            // splitContainerControl2
+            // 
+            this.splitContainerControl2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerControl2.Horizontal = false;
+            this.splitContainerControl2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainerControl2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.splitContainerControl2.Name = "splitContainerControl2";
+            this.splitContainerControl2.Panel1.Controls.Add(this.groupControl1);
+            this.splitContainerControl2.Panel1.Text = "Panel1";
+            this.splitContainerControl2.Panel2.Controls.Add(this.gcPregunta);
+            this.splitContainerControl2.Panel2.Text = "Panel2";
+            this.splitContainerControl2.Size = new System.Drawing.Size(1129, 604);
+            this.splitContainerControl2.SplitterPosition = 68;
+            this.splitContainerControl2.TabIndex = 0;
+            this.splitContainerControl2.Text = "splitContainerControl2";
+            // 
+            // groupControl1
+            // 
+            this.groupControl1.Controls.Add(this.chkSelecciona);
+            this.groupControl1.Controls.Add(this.btnBuscar);
+            this.groupControl1.Controls.Add(this.txtDescripcion);
+            this.groupControl1.Controls.Add(this.labelControl1);
+            this.groupControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupControl1.Location = new System.Drawing.Point(0, 0);
+            this.groupControl1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupControl1.Name = "groupControl1";
+            this.groupControl1.Size = new System.Drawing.Size(1129, 78);
+            this.groupControl1.TabIndex = 34;
+            this.groupControl1.Text = "Criterios de Búsqueda";
+            // 
+            // chkSelecciona
+            // 
+            this.chkSelecciona.Location = new System.Drawing.Point(980, 39);
+            this.chkSelecciona.Name = "chkSelecciona";
+            this.chkSelecciona.Properties.Caption = "Seleccionar Todos";
+            this.chkSelecciona.Size = new System.Drawing.Size(139, 20);
+            this.chkSelecciona.TabIndex = 132;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnBuscar.ImageOptions.Image")));
+            this.btnBuscar.Location = new System.Drawing.Point(449, 36);
+            this.btnBuscar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(78, 23);
+            this.btnBuscar.TabIndex = 29;
+            this.btnBuscar.Text = "Buscar";
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(81, 37);
+            this.txtDescripcion.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Properties.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtDescripcion.Properties.MaxLength = 50;
+            this.txtDescripcion.Size = new System.Drawing.Size(362, 22);
+            this.txtDescripcion.TabIndex = 28;
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(5, 40);
+            this.labelControl1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(70, 16);
+            this.labelControl1.TabIndex = 27;
+            this.labelControl1.Text = "Descripción:";
+            // 
+            // gcPregunta
+            // 
+            this.gcPregunta.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gcPregunta.EmbeddedNavigator.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.gcPregunta.Location = new System.Drawing.Point(0, 0);
+            this.gcPregunta.MainView = this.gvPregunta;
+            this.gcPregunta.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.gcPregunta.Name = "gcPregunta";
+            this.gcPregunta.RepositoryItems.AddRange(new DevExpress.XtraEditors.Repository.RepositoryItem[] {
+            this.repositoryItemCheckEdit1});
+            this.gcPregunta.Size = new System.Drawing.Size(1129, 530);
+            this.gcPregunta.TabIndex = 37;
+            this.gcPregunta.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gvPregunta});
+            // 
+            // gvPregunta
+            // 
+            this.gvPregunta.Appearance.ViewCaption.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold);
+            this.gvPregunta.Appearance.ViewCaption.ForeColor = System.Drawing.Color.Navy;
+            this.gvPregunta.Appearance.ViewCaption.Options.UseFont = true;
+            this.gvPregunta.Appearance.ViewCaption.Options.UseForeColor = true;
+            this.gvPregunta.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.gridColumn2,
+            this.gridColumn5,
+            this.gridColumn1,
+            this.gridColumn3,
+            this.gridColumn7,
+            this.gridColumn4});
+            this.gvPregunta.GridControl = this.gcPregunta;
+            this.gvPregunta.Name = "gvPregunta";
+            this.gvPregunta.OptionsView.AllowCellMerge = true;
+            this.gvPregunta.OptionsView.ColumnAutoWidth = false;
+            this.gvPregunta.OptionsView.ShowGroupPanel = false;
+            this.gvPregunta.OptionsView.ShowViewCaption = true;
+            this.gvPregunta.ViewCaption = "LISTADO DE PREGUNTAS DEL CUESTIONARIO";
+            // 
+            // gridColumn2
+            // 
+            this.gridColumn2.Caption = "IdEmpresa";
+            this.gridColumn2.FieldName = "IdEmpresa";
+            this.gridColumn2.Name = "gridColumn2";
+            this.gridColumn2.OptionsColumn.AllowEdit = false;
+            this.gridColumn2.OptionsColumn.AllowFocus = false;
+            // 
+            // gridColumn1
+            // 
+            this.gridColumn1.Caption = "IdTema";
+            this.gridColumn1.FieldName = "IdTema";
+            this.gridColumn1.Name = "gridColumn1";
+            this.gridColumn1.OptionsColumn.AllowEdit = false;
+            this.gridColumn1.OptionsColumn.AllowFocus = false;
+            this.gridColumn1.Width = 90;
+            // 
+            // gridColumn5
+            // 
+            this.gridColumn5.Caption = "IdPregunta";
+            this.gridColumn5.FieldName = "IdPregunta";
+            this.gridColumn5.Name = "gridColumn5";
+            this.gridColumn5.OptionsColumn.AllowEdit = false;
+            this.gridColumn5.OptionsColumn.AllowFocus = false;
+            // 
+            // gridColumn3
+            // 
+            this.gridColumn3.Caption = "IdCuestionario";
+            this.gridColumn3.FieldName = "IdCuestionario";
+            this.gridColumn3.Name = "gridColumn3";
+            this.gridColumn3.OptionsColumn.AllowEdit = false;
+            this.gridColumn3.OptionsColumn.AllowFocus = false;
+            this.gridColumn3.OptionsColumn.AllowMerge = DevExpress.Utils.DefaultBoolean.False;
+            this.gridColumn3.Width = 300;
+            // 
+            // repositoryItemCheckEdit1
+            // 
+            this.repositoryItemCheckEdit1.AutoHeight = false;
+            this.repositoryItemCheckEdit1.CheckStyle = DevExpress.XtraEditors.Controls.CheckStyles.UserDefined;
+            this.repositoryItemCheckEdit1.ImageOptions.ImageChecked = global::SSOMA.Presentacion.Properties.Resources.Persona_16x16;
+            this.repositoryItemCheckEdit1.ImageOptions.ImageGrayed = global::SSOMA.Presentacion.Properties.Resources.Persona_16x16;
+            this.repositoryItemCheckEdit1.ImageOptions.ImageUnchecked = global::SSOMA.Presentacion.Properties.Resources.Persona_16x16;
+            this.repositoryItemCheckEdit1.Name = "repositoryItemCheckEdit1";
+            // 
+            // gridColumn7
+            // 
+            this.gridColumn7.Caption = "Descripción de la Pregunta";
+            this.gridColumn7.FieldName = "DescPregunta";
+            this.gridColumn7.Name = "gridColumn7";
+            this.gridColumn7.OptionsColumn.AllowEdit = false;
+            this.gridColumn7.OptionsColumn.AllowFocus = false;
+            this.gridColumn7.OptionsColumn.AllowMerge = DevExpress.Utils.DefaultBoolean.False;
+            this.gridColumn7.Visible = true;
+            this.gridColumn7.VisibleIndex = 0;
+            this.gridColumn7.Width = 900;
+            // 
+            // gridColumn4
+            // 
+            this.gridColumn4.Caption = "Puntaje";
+            this.gridColumn4.FieldName = "Punta";
+            this.gridColumn4.Name = "gridColumn4";
+            this.gridColumn4.OptionsColumn.AllowEdit = false;
+            this.gridColumn4.OptionsColumn.AllowFocus = false;
+            this.gridColumn4.OptionsColumn.AllowMerge = DevExpress.Utils.DefaultBoolean.False;
+            this.gridColumn4.Visible = true;
+            this.gridColumn4.VisibleIndex = 1;
+            this.gridColumn4.Width = 100;
+            // 
+            // imgLista
+            // 
+            this.imgLista.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgLista.ImageStream")));
+            this.imgLista.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgLista.Images.SetKeyName(0, "Empresa_16x16.gif");
+            this.imgLista.Images.SetKeyName(1, "CategoriaTema_16x16.gif");
+            this.imgLista.Images.SetKeyName(2, "Tema_16x16.gif");
+            this.imgLista.Images.SetKeyName(3, "Cuestionario_16x16.gif");
+            // 
+            // frmManPregunta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1752, 634);
+            this.Controls.Add(this.splitContainerControl1);
+            this.Controls.Add(this.tlbMenu);
+            this.Name = "frmManPregunta";
             this.Text = "frmManPregunta";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl1)).EndInit();
+            this.splitContainerControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl2)).EndInit();
+            this.splitContainerControl2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
+            this.groupControl1.ResumeLayout(false);
+            this.groupControl1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chkSelecciona.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtDescripcion.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gcPregunta)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvPregunta)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemCheckEdit1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private ControlUser.UIToolBar tlbMenu;
+        private DevExpress.XtraEditors.SplitContainerControl splitContainerControl1;
+        private System.Windows.Forms.TreeView tvwDatos;
+        private DevExpress.XtraEditors.SplitContainerControl splitContainerControl2;
+        private DevExpress.XtraEditors.GroupControl groupControl1;
+        private DevExpress.XtraEditors.CheckEdit chkSelecciona;
+        private DevExpress.XtraEditors.SimpleButton btnBuscar;
+        private DevExpress.XtraEditors.TextEdit txtDescripcion;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraGrid.GridControl gcPregunta;
+        private DevExpress.XtraGrid.Views.Grid.GridView gvPregunta;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn2;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn1;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn5;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn3;
+        private DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit repositoryItemCheckEdit1;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn7;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn4;
+        private System.Windows.Forms.ImageList imgLista;
     }
 }
